68fb9e3f7ad982591de62e7c154df677cda9c569
Mr.Owl <wangdian2018@gmail.com>
2022-02-28 09:54:48 +0800

    最长回文字符串

--- 模拟/longestPalindrome.py
+++ 模拟/longestPalindrome.py
@@ -1,37 +1,38 @@
-class Solution:
-    def longestPalindrome(self, s: str) -> str:
-        n = len(s)
-        if n < 2:
-            return s
-
-        max_len = 1
-        begin = 0
-        # dp[i][j] 表示 s[i..j] 是否是回文串
-        dp = [[False] * n for _ in range(n)]
-        for i in range(n):
-            dp[i][i] = True
-
-        # 递推开始
-        # 先枚举子串长度
-        for L in range(2, n + 1):
-            # 枚举左边界，左边界的上限设置可以宽松一些
-            for i in range(n):
-                # 由 L 和 i 可以确定右边界，即 j - i + 1 = L 得
-                j = L + i - 1
-                # 如果右边界越界，就可以退出当前循环
-                if j >= n:
-                    break
-
-                if s[i] != s[j]:
-                    dp[i][j] = False
-                else:
-                    if j - i < 3:
-                        dp[i][j] = True
+s = "accddada"
+ss = ""
+ans = []
+r = s[::-1]
+maxnum = 1
+res = 1
+n = len(s)
+dp = [[False] * n for _ in range(n)]
+print(dp)
+if len(r) == 1:
+    print(r)
+if len(r) == 0:
+    print([])
+else:
+    for i in range(len(r) - 1):
+        if r[i] + r[i + 1] in s:
+            ss = r[i] + r[i + 1]
+            num = i + 2
+            while True:
+                if num < len(r):
+                    ss = ss + r[num]
+                    if ss in s:
+                        num += 1
                     else:
-                        dp[i][j] = dp[i + 1][j - 1]
+                        ss = ss[:-1]
+                        ans.append(ss)
+                        break
+                else:
+                    break
+        else:
+            if i + 1 ==len(r) - 1:
+                print(r[i])
+    for i, num in enumerate(ans):
+        if len(num) > maxnum:
+            maxnum = len(num)
+            res = i
+    print(ans[res])
 
-                # 只要 dp[i][L] == true 成立，就表示子串 s[i..L] 是回文，此时记录回文长度和起始位置
-                if dp[i][j] and j - i + 1 > max_len:
-                    max_len = j - i + 1
-                    begin = i
-        return s[begin:begin + max_len]
