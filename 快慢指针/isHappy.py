42e0a67caef1e1e590d537e229b4beb1d472806b
Mr.Owl <wangdian2018@gmail.com>
2022-03-03 11:40:21 +0800

    快乐数（简单）

---
+++ 快慢指针/isHappy.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        def get_next(number):
+            total_sum = 0
+            while number > 0:
+                number, digit = divmod(number, 10)
+                total_sum += digit ** 2
+            return total_sum
+
+
+        turtle = n
+        rabbit = get_next(n)
+        while rabbit != 1 and turtle != rabbit:
+            turtle = get_next(turtle)
+            rabbit = get_next(get_next(rabbit))
+        return rabbit == 1
